8342ecd861fc3467cf09b955a71a05d293a44bc4
SimonLepage13 <simon.lepage.3@ulaval.ca>
2022-11-21 13:33:22 -0500

    Initial commit

---
+++ ZYBO_HDMI_MGA_2018_2/project_1/project_1.srcs/sources_1/bd/HDMI_bd/ip/HDMI_bd_clk_wiz_0_0/HDMI_bd_clk_wiz_0_0_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.2.1 (win64) Build 2288692 Thu Jul 26 18:24:02 MDT 2018
+-- Date        : Thu Oct 20 10:02:33 2022
+-- Host        : pcetu-195 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               U:/FPGA/fpga_lab3/ZYBO_HDMI_MGA_2018_2/project_1/project_1.srcs/sources_1/bd/HDMI_bd/ip/HDMI_bd_clk_wiz_0_0/HDMI_bd_clk_wiz_0_0_stub.vhdl
+-- Design      : HDMI_bd_clk_wiz_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity HDMI_bd_clk_wiz_0_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end HDMI_bd_clk_wiz_0_0;
+
+architecture stub of HDMI_bd_clk_wiz_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,reset,locked,clk_in1";
+begin
+end;
